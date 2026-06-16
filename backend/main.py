c434baa909fd6c60e4dc6872da1719e7cfe3aae1
Guilherme Aguiar <guilherme.ghca@hotmail.com>
2026-06-15 23:05:50 -0300

    fix: remove bcrypt__backend de CryptContext

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__backend="bcrypt")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
